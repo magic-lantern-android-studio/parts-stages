421cc3d3c51bb66789626fd03c18f30d59807353
Mark Millard <msm@wizzerworks.com>
2016-09-22 11:58:48 -0600

    Updating library dependencies. Added OpenGL error checking utility.

--- app/src/main/java/com/wizzer/mle/opengl/GLES20Renderer.java
+++ app/src/main/java/com/wizzer/mle/opengl/GLES20Renderer.java
@@ -62,4 +62,28 @@ public class GLES20Renderer extends GLRenderer
             }
         }
     }
+
+    /**
+     * Utility method for debugging OpenGL calls. Provide the name of the call
+     * just after making it:
+     *
+     * <pre>
+     * mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+     * MyGLRenderer.checkGlError("glGetUniformLocation");
+     * </pre>
+     *
+     * If the operation is not successful, the check throws an error.
+     *
+     * @param glOperation Name of the OpenGL call to check.
+     */
+    public static void checkGlError(String glOperation)
+    {
+        int error;
+
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR)
+        {
+            Log.e(MleTitle.DEBUG_TAG, glOperation + ": glError " + error);
+            throw new RuntimeException(glOperation + ": glError " + error);
+        }
+    }
 }
